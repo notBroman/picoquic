0d1afe1c5900b6d003566fbe63d87205f971df63
Roman Berger <romanberger001@gmail.com>
2024-04-11 17:51:20 +0200

    simulation arg parsing

---
+++ simulator/sim.c
@@ -0,0 +1,13 @@
+#ifdef _WINDOWS
+#include "getopt.h"
+#endif
+#include "picoquic.h"
+#include "picoquic_utils.h"
+#include "picoquictest.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv){
+    printf("CUSTOM SIMULATOR");
+}
